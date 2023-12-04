b09d7accd9577b0b70b71ebfb871877b4484aa24
Muhammad Aaqil <aaqilcs102@gmail.com>
2023-12-04 22:25:05 +0500

    feat: enable cache decorator to receive datasource
    Signed-off-by: Muhammad Aaqil <aaqilcs102@gmail.com>

--- src/decorators/cache.decorator.ts
+++ src/decorators/cache.decorator.ts
@@ -2,14 +2,16 @@ import { MethodDecoratorFactory, Constructor, MetadataInspector } from "@loopbac
 import { CACHE_METADATA_KEY } from '../keys';
 
 export interface CacheMetadata {
+  datasource: string;
   ttl: number;
   options?: Object;
 }
 
-export function cache(ttl: number = 60, options?: Object) {
+export function cache(datasource: string, ttl: number = 60, options?: Object) {
   return MethodDecoratorFactory.createDecorator<CacheMetadata>(
     CACHE_METADATA_KEY,
     {
+      datasource,
       ttl,
       options: options || {}
     }
